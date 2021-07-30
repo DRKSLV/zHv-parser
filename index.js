cb1883b259352b6fd7553fd3b937e5c1bb6632b5
DRKSLV <56208328+DRKSLV@users.noreply.github.com>
2021-07-30 03:35:03 +0200

    Organize my madness imperative code

--- index.js
+++ index.js
@@ -1,26 +1,226 @@
-const fs = require("fs");
-const data = fs.readFileSync( "./rawdata/"+process.argv[2] ).toString("utf8");
-
+// NOTES:
 // command: node . zHV_aktuell_csv.2021-07-29.csv
 
-/**
- * Alle Haltestellen 
- * @type {Object.<string, Haltestelle>}
- */
+/// DEPENDENCIES
+const fs = require("fs");
+
+
+/// GLOBALS
+/** Alle Haltestellen @type {Object.<string, Haltestelle>} */
 const Stations = {};
 
-/**
- * Alle Gemeinden
- * @type {Object.<number, Municipality>}
- */
+/** Alle Gemeinden @type {Object.<number, Municipality>} */
 const Municipalities = {};
 
 /** @type {Array.<String>} */
 const Authorities = [];
 
-// 7, 8
-var anzStationen = 0, anzMasten = 0, anzBereiche = 0;
 
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+//                                                  TOP LEVEL IMPERATIVE EXECUTION BLOCK
+
+const data = fs.readFileSync( "./rawdata/"+process.argv[2] ).toString("utf8");
+parseData(data);
+
+console.log("Saving...")
+fs.writeFileSync("./refineddata/stations.json", JSON.stringify(Stations));
+fs.writeFileSync("./refineddata/municipalities.json", JSON.stringify(Municipalities));
+fs.writeFileSync("./refineddata/authorities.json", JSON.stringify(Authorities));
+
+console.log("Saving Debug...")
+
+
+fs.writeFileSync("./refineddata/stationsDebug.json", debugJSON(Stations));
+fs.writeFileSync("./refineddata/municipalitiesDebug.json", debugJSON(Municipalities));
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// DEFINITIONS
+/// THE Function
+function parseData(data) {
+    var anzStationen = 0, anzMasten = 0, anzBereiche = 0;
+
+    // ROWS
+    var rows = data.split("\n");
+    console.log("Rows: ", rows.length);
+
+    // KEYS
+    var keys = [];
+
+    rows.forEach( (row, rowNum) => {
+        row = row.replace("\r", "") ;
+
+        /** @type {Haltestelle} */
+        var entryObject = {
+            c: [0, 0] // to be able to assign without error
+        };
+        var entryType;
+        var entryParent;
+        var entryId;
+        var municipality = {};
+        var district = {};
+
+        row.slice(1, row.length-1).split('";"')
+        .forEach((field, fieldNum) => {
+            //KEYS: find  
+            if(rowNum===0) {
+                keys.push(field);
+                return;
+            }
+
+            // ATTRIBUTES
+            // current key
+            const key = keys[fieldNum];
+            // filter non-values
+            if (field === "") field = undefined;
+            if (field==="-") field= undefined;
+
+            // assign field to current object
+            switch (key) {
+                case "DHID":
+                    entryObject.i = field.split(":").slice(2).reduce((p, c, i, a) => i>0?p+":"+c:c, "")
+                    return entryId = field;
+                case "Name":
+                    return entryObject.n = field;
+                case "Longitude":
+                    return entryObject.c[1] = Number.parseFloat(field.replace(",", "."));
+                case "Latitude":
+                    return entryObject.c[0] = Number.parseFloat(field.replace(",", "."));
+                case "Authority":
+                    var idx = Authorities.indexOf(field);
+                    if(idx === -1){
+                        idx = Authorities.length;
+                        Authorities.push(field);
+                    }
+                    return entryObject.a = idx;
+                case "Description":
+                    return entryObject.des = field;
+                case "Condition":
+                    return entryObject.s = field==="Served" ? undefined : false;
+                case "MunicipalityCode":
+                    municipality.key = Number.parseInt(field);
+                    return entryObject.m = Number.parseInt(field);
+                case "DistrictCode":
+                    district.key = Number.parseInt(field);
+                    return entryObject.d = Number.parseInt(field) || undefined;
+                case "Municipality":
+                    return municipality.val = field;
+                case "District":
+                    return district.val = field;
+                case "Type":
+                    return entryType= field;
+                case "Parent":
+                    return entryParent= field;
+            }
+        });
+
+        //KEYS: found
+        if(rowNum===0) {
+            console.log("Keys: ", keys);
+            return;
+        }
+
+        //ENTRY: (type?)
+        
+        if(entryType==="A" || entryType==="Q") { // remove duplicate info 
+            delete entryObject.i;
+            delete entryObject.a;
+            delete entryObject.d;
+            delete entryObject.m;
+        }
+
+
+        switch(entryType) {
+            case "S": // HAltestelle
+                Stations[entryId] = entryObject;
+                anzStationen++;
+                break;
+            case "A": // bereich
+                var splitt = entryId.split(":");
+                var myid = splitt[splitt.length-1];
+                var parent = Stations[entryParent] || {};
+
+                Stations[entryParent] = {
+                    ...parent,
+                    b: { ...parent.b , [myid]: entryObject}
+                }
+
+                anzBereiche++;
+                break;
+            case "Q": // mast/steig
+                var splitt = entryId.split(":");
+                var superParentId = splitt.slice(0, 3).reduce((p, c, i, a) => i>0?p+":"+c:c, "") ;
+                var parentId = splitt[splitt.length-2] || 0;
+                var myid = splitt[splitt.length-1];
+                //console.log(`id: ${entryId}; my: ${myid}; parent: ${parentId}; superParent: ${superParentId}` )
+
+                var superParent = Stations[superParentId] || {};
+                var parent = (superParent.b || {})[parentId] || {}
+
+                Stations[superParentId] = {
+                    ...superParent,
+                    b: {
+                        ...superParent.b || {},
+                        [parentId]: {
+                            ...parent,
+                            g: {
+                                ...parent.g || {},
+                                [myid]: entryObject
+                            }
+                        }
+                    }
+                };
+                anzMasten++;
+                break;
+        }
+
+        // assign extracted municipality
+        if(municipality.val){
+            var bef = Municipalities[municipality.key] || {};
+            var befD = bef.d || {};
+        
+            if(district.val)
+                befD[district.key] = district.val;
+
+            var befA =(bef.a || []);
+            if(!befA.includes(entryObject.a)) {
+                befA.push(entryObject.a)
+            }
+            
+            Municipalities[municipality.key] = {
+                n: municipality.val,
+                d: befD,
+                a: befA
+            }
+        }
+    })
+
+    console.log(
+    `Converted:
+        ${anzStationen} Stationen
+        ${anzMasten} H-Masten/Gleise
+        ${anzBereiche} Bereiche
+    In: 
+        ${Object.keys(Municipalities).length} Gemeinden
+    `)
+}
+
+function debugJSON(obj) {
+    if(typeof obj !== "object") return console.error("Must be object");
+
+    var count=0;
+    var max = Object.keys(obj).length;
+
+    var out = "{";
+    for ( let i in obj ) {
+        count++;
+        out += `"${i}":${JSON.stringify( obj[i] )} ${count<max?",":""} ${(count<300||count>max-300)?"\n":""}`
+    }
+    out+="}";
+
+    return out;
+}
+
+//// TYPES
 /**
  * @typedef Haltestelle
  * @property {String} n name
@@ -57,198 +257,3 @@ var anzStationen = 0, anzMasten = 0, anzBereiche = 0;
  * @property {Object.<Number, String>} d districts
  * @property {Array.<String>} a authorities reporting stops for this Municipality
  */
-
-
-
-// ROWS
-var rows = data.split("\n");
-console.log("Rows: ", rows.length);
-
-// KEYS
-var keys = [];
-
-rows.forEach( (row, rowNum) => {
-    row = row.replace("\r", "") ;
-
-    /** @type {Haltestelle} */
-    var entryObject = {
-        c: [0, 0] // to be able to assign without error
-    };
-    var entryType;
-    var entryParent;
-    var entryId;
-    var municipality = {};
-    var district = {};
-
-    row.slice(1, row.length-1).split('";"')
-    .forEach((field, fieldNum) => {
-        //KEYS: find  
-        if(rowNum===0) {
-            keys.push(field);
-            return;
-        }
-
-        // ATTRIBUTES
-        // current key
-        const key = keys[fieldNum];
-        // filter non-values
-        if (field === "") field = undefined;
-        if (field==="-") field= undefined;
-
-        // assign field to current object
-        switch (key) {
-            case "DHID":
-                entryObject.i = field.split(":").slice(2).reduce((p, c, i, a) => i>0?p+":"+c:c, "")
-                return entryId = field;
-            case "Name":
-                return entryObject.n = field;
-            case "Longitude":
-                return entryObject.c[1] = Number.parseFloat(field.replace(",", "."));
-            case "Latitude":
-                return entryObject.c[0] = Number.parseFloat(field.replace(",", "."));
-            case "Authority":
-                var idx = Authorities.indexOf(field);
-                if(idx === -1){
-                    idx = Authorities.length;
-                    Authorities.push(field);
-                }
-                return entryObject.a = idx;
-            case "Description":
-                return entryObject.des = field;
-            case "Condition":
-                return entryObject.s = field==="Served" ? undefined : false;
-            case "MunicipalityCode":
-                municipality.key = Number.parseInt(field);
-                return entryObject.m = Number.parseInt(field);
-            case "DistrictCode":
-                district.key = Number.parseInt(field);
-                return entryObject.d = Number.parseInt(field) || undefined;
-            case "Municipality":
-                return municipality.val = field;
-            case "District":
-                return district.val = field;
-            case "Type":
-                return entryType= field;
-            case "Parent":
-                return entryParent= field;
-        }
-    });
-
-    //KEYS: found
-    if(rowNum===0) {
-        console.log("Keys: ", keys);
-        return;
-    }
-
-    //ENTRY: (type?)
-    
-    if(entryType==="A" || entryType==="Q") { // remove duplicate info 
-        delete entryObject.i;
-        delete entryObject.a;
-        delete entryObject.d;
-        delete entryObject.m;
-    }
-
-
-    switch(entryType) {
-        case "S": // HAltestelle
-            Stations[entryId] = entryObject;
-            anzStationen++;
-            break;
-        case "A": // bereich
-            var splitt = entryId.split(":");
-            var myid = splitt[splitt.length-1];
-            var parent = Stations[entryParent] || {};
-
-            Stations[entryParent] = {
-                ...parent,
-                b: { ...parent.b , [myid]: entryObject}
-            }
-
-            anzBereiche++;
-            break;
-        case "Q": // mast/steig
-            var splitt = entryId.split(":");
-            var superParentId = splitt.slice(0, 3).reduce((p, c, i, a) => i>0?p+":"+c:c, "") ;
-            var parentId = splitt[splitt.length-2] || 0;
-            var myid = splitt[splitt.length-1];
-            //console.log(`id: ${entryId}; my: ${myid}; parent: ${parentId}; superParent: ${superParentId}` )
-
-            var superParent = Stations[superParentId] || {};
-            var parent = (superParent.b || {})[parentId] || {}
-
-            Stations[superParentId] = {
-                ...superParent,
-                b: {
-                    ...superParent.b || {},
-                    [parentId]: {
-                        ...parent,
-                        g: {
-                            ...parent.g || {},
-                            [myid]: entryObject
-                        }
-                    }
-                }
-            };
-            anzMasten++;
-            break;
-    }
-
-    // assign extracted municipality
-    if(municipality.val){
-        var bef = Municipalities[municipality.key] || {};
-        var befD = bef.d || {};
-    
-        if(district.val)
-            befD[district.key] = district.val;
-
-        var befA =(bef.a || []);
-        if(!befA.includes(entryObject.a)) {
-            befA.push(entryObject.a)
-        }
-        
-        Municipalities[municipality.key] = {
-            n: municipality.val,
-            d: befD,
-            a: befA
-        }
-    }
-})
-console.log(
-`Converted:
-    ${anzStationen} Stationen
-    ${anzMasten} H-Masten/Gleise
-    ${anzBereiche} Bereiche
-In: 
-    ${Object.keys(Municipalities).length} Gemeinden
-`)
-
-
-console.log("Saving...")
-fs.writeFileSync("./refineddata/stations.json", JSON.stringify(Stations));
-fs.writeFileSync("./refineddata/municipalities.json", JSON.stringify(Municipalities));
-fs.writeFileSync("./refineddata/authorities.json", JSON.stringify(Authorities));
-
-console.log("Saving Debug...")
-
-
-fs.writeFileSync("./refineddata/stationsDebug.json", debugJSON(Stations));
-fs.writeFileSync("./refineddata/municipalitiesDebug.json", debugJSON(Municipalities));
-
-
-
-function debugJSON(obj) {
-    if(typeof obj !== "object") return console.error("Must be object");
-
-    var count=0;
-    var max = Object.keys(obj).length;
-
-    var out = "{";
-    for ( let i in obj ) {
-        count++;
-        out += `"${i}":${JSON.stringify( obj[i] )} ${count<max?",":""} ${(count<300||count>max-300)?"\n":""}`
-    }
-    out+="}";
-
-    return out;
-}
